5883d176718c5ca806b6e559a59182861804425c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:35:49 +0100

    Scrittura codice iniziale

--- ConsoleLavoroGit/Program.cs
+++ ConsoleLavoroGit/Program.cs
@@ -10,7 +10,17 @@ namespace ConsoleLavoroGit
     {
         static void Main(string[] args)
         {
-           
+            string risultato;
+            int num = 255;
+            risultato = "";
+            while (num > 1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"Il numero binario è {risultato}");
         }
     }
 }
